c61584d258952440f750bca82d0d7aa519190bda
Dr-Noob <peibolms@gmail.com>
2021-11-06 16:27:06 +0100

    Completed basic script to decrypt xml file

--- decrypt.py
+++ decrypt.py
@@ -11,7 +11,32 @@ def xor(a, b):
         xored.append(chr(xored_value))
     return ''.join(xored)
 
-input="KgU/Ixc/BxQ7EAE5NQ0kKA=="
-key = bytearray([0x46, 0x64, 0x4b])
+if(len(sys.argv) != 2):
+    print("ERROR: Need one arg")
+    print("Use:",sys.argv[0],"file")
+    sys.exit(1)
 
-print(xor(base64.b64decode(input), key))
+try:
+    input_f = open(sys.argv[1],'r')
+except FileNotFoundError:
+    print ("ERROR: File '",sys.argv[1],"' does not exist")
+    sys.exit(1)
+
+regex_item = re.compile(r'\s*<string\s+name="([^"]+)">([^<]+)</string>')
+
+sys.stdout.write("<?xml version='1.0' encoding='utf-8' standalone='yes' ?>\n")
+sys.stdout.write("<map>\n")
+
+# XOR key is 0x46644b
+xor_key = bytearray([0x46, 0x64, 0x4b])
+
+for line in input_f:
+    if regex_item.match(line):
+        m = re.search(regex_item, line)
+        sys.stdout.write("    <string name=\"")
+        sys.stdout.write(xor(base64.b64decode(m.group(1)), xor_key))
+        sys.stdout.write("\">")
+        sys.stdout.write(xor(base64.b64decode(m.group(2)), xor_key))
+        sys.stdout.write("</string>\n")
+
+sys.stdout.write("</map>\n")
