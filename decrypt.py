77cd264bdbdcfd1a53d83243a13ad831423e50e4
Dr-Noob <peibolms@gmail.com>
2021-11-06 16:21:08 +0100

    Refactor initial code

--- decrypt.py
+++ decrypt.py
@@ -4,22 +4,14 @@ import sys
 import re
 import base64
 
-def encrypt3(a):
-    barr = [0x46, 0x64, 0x4b]
-    b = bytearray(barr)
-    print("a=", type(a))
-    print("b=", type(b))
+def xor(a, b):
     xored = []
     for i in range(max(len(a), len(b))):
-        print("xor ", hex(a[i%len(a)]), " with ", hex(b[i%len(b)]))
         xored_value = a[i%len(a)] ^ b[i%len(b)]
         xored.append(chr(xored_value))
-
     return ''.join(xored)
 
 input="KgU/Ixc/BxQ7EAE5NQ0kKA=="
+key = bytearray([0x46, 0x64, 0x4b])
 
-my_str_as_bytes = base64.b64decode(input)
-print(my_str_as_bytes)
-
-print(encrypt3(my_str_as_bytes))
+print(xor(base64.b64decode(input), key))
