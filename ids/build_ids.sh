5fa830eb0b0c02e84c201915c852bf24af7ca2c8
Dr-Noob <peibolms@gmail.com>
2022-01-22 10:04:28 +0100

    [v1.1.3] Make build_ids retry the connection if the server returns an error

--- ids/build_ids.sh
+++ ids/build_ids.sh
@@ -4,19 +4,31 @@ function scrap_ids() {
   UA=""
   next_page=1
   p=1
+  max_retries=3
+  r=0
 
   while [ $next_page -eq 1 ]
   do
-    wget --user-agent="$UA" "https://www.fifplay.com/fifa-22/players/?position=&quality=&type=$3&squad=&status=&nation=&league=&club=&sort=&foot=&minrating=$2&maxrating=$1&search=&page=$p" -O fifplay.html
+    while true; do
+      let r=r+1
+      wget --user-agent="$UA" "https://www.fifplay.com/fifa-22/players/?position=&quality=&type=$3&squad=&status=&nation=&league=&club=&sort=&foot=&minrating=$2&maxrating=$1&search=&page=$p" -O fifplay.html
+      sleep $((1 + $RANDOM % 2))
+      [[ $? -ne 0 && $r -le $max_retries ]] || break
+    done
+
+    if [[ $r -eq $max_retries ]]; then
+      echo 'Too much errors, exiting...'
+      exit 1
+    fi
+
     grep '<div><a href="https://www.fifplay.com/fifa-22/players/' fifplay.html | cut -d'/' -f6 >> "$4"
 
+    let r=0
     let "p=p+1"
     if ! grep -q "page=$p" fifplay.html
     then
       next_page=0
     fi
-
-    sleep $((1 + $RANDOM % 5))
   done
 }
 
